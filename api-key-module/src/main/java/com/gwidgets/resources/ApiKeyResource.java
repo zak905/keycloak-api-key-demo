1c399db9435fa0070cfb620e8bf3acbc32aef273
aprat <a.prat@show-you.fr>
2023-06-07 14:58:15 +0200

    Replace checkApiKey endpoint to getApiKey
    Returns the api key of the user with specific role and name

--- api-key-module/src/main/java/com/gwidgets/resources/ApiKeyResource.java
+++ api-key-module/src/main/java/com/gwidgets/resources/ApiKeyResource.java
@@ -15,17 +15,26 @@ public class ApiKeyResource {
     private KeycloakSession session;
 
     private final String realmName;
+    private final String roleName;
 
     public ApiKeyResource(KeycloakSession session) {
         this.session = session;
         String envRealmName = System.getenv("REALM_NAME");
+        String envRoleName = System.getenv("ROLE_NAME_ALLOWED");
         this.realmName = Objects.isNull(envRealmName) || Objects.equals(System.getenv(envRealmName), "")? "example": envRealmName;
+        this.roleName =  Objects.isNull(envRoleName) || Objects.equals(System.getenv(envRoleName), "")? "example": envRoleName;
     }
 
     @GET
     @Produces("application/json")
-    public Response checkApiKey(@QueryParam("apiKey") String apiKey) {
-        List<UserModel> result = session.userStorageManager().searchForUserByUserAttribute("api-key", apiKey, session.realms().getRealm(realmName));
-        return result.isEmpty() ? Response.status(401).type(MediaType.APPLICATION_JSON).build(): Response.ok().type(MediaType.APPLICATION_JSON).build();
+    public Response getApiKey(@QueryParam("name") String userName) {
+        UserModel user = session.userStorageManager().getUserByUsername(session.realms().getRealm(realmName), userName);
+
+            if(user != null && user.isEnabled() && user.getRoleMappingsStream().anyMatch(r -> r.getName().equals(roleName)) && user.getAttributeStream("api-key").count() == 1)
+                return user.getAttributeStream("api-key").findFirst()
+                        .map(key -> Response.ok(key).type(MediaType.APPLICATION_JSON).build())
+                        .orElseGet(() -> Response.status(401).type(MediaType.APPLICATION_JSON).build());
+
+        return Response.status(401).type(MediaType.APPLICATION_JSON).build();
     }
 }
